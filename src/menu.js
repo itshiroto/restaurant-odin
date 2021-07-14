1cd7c8f850f328c05e8e757122cd67889d578139
Rivo Wowor <rivowowor7@gmail.com>
2021-07-14 21:51:43 +0800

    M: Restructuring menu.js functions into object

--- src/menu.js
+++ src/menu.js
@@ -8,35 +8,39 @@ function init() {
 	return grid;
 }
 
-function addMenu(item) {
-	const card = document.createElement('div');
-	const name = document.createElement('h2');
-	const desc = document.createElement('p');
-	const txtParent = document.createElement('div');
-
-	name.innerText = item.name;
-	desc.innerText = item.desc;
-
-	card.classList.add("menu-item")
-	name.classList.add("item-name");
-	desc.classList.add("item-desc");
-	txtParent.classList.add("item-text");
-
-	txtParent.append(name, desc);
-	if (item.img){
-		const img = new Image();
-		img.src = require('./img/' + item.img);
-		card.prepend(img)
+const card = (() => {
+	const addText = (name, desc) => {
+		const base = document.createElement('div');
+		const nameDOM = document.createElement('h2');
+		const descDOM = document.createElement('p');
+		nameDOM.innerText = name;
+		descDOM.innerText = desc;
+		nameDOM.classList.add("item-name");
+		descDOM.classList.add("item-desc");
+		base.classList.add("item-text");
+		base.append(nameDOM, descDOM);
+		return base;
 	}
-	card.append(txtParent);
-	return card;
-}
+	const addImg = (img) => {
+		if (img == undefined) return "";
+		const imgDOM = new Image();
+		imgDOM.src = require('./img/' + img);
+		return imgDOM;
+	}
+	const create = (item) => {
+		const card = document.createElement('div');
+		card.classList.add("menu-item")
+		card.append(addImg(item.img),addText(item.name, item.desc));
+		return card;
+	}
+	return {
+		create,
+	}
+})()
 
 function render() {
 	const base = init();
-	menu.forEach(item => {
-		base.appendChild(addMenu(item));
-	})
+	menu.forEach(item => base.appendChild(card.create(item)))
 	const container = createContainer();
 	container.appendChild(base);
 	return container;
