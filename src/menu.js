65077deb29871dd8af6978750791c412c009ccae
Junichirou Hiroto <rivowowor7@gmail.com>
2021-07-16 10:43:22 +0800

    M: Completed menu implementation

--- src/menu.js
+++ src/menu.js
@@ -9,17 +9,30 @@ function init() {
 }
 
 const card = (() => {
-	const addText = (name, desc) => {
-		const base = createDOM('div', "item-text");
+	const addTitle = (name, price) => {
+		const parent = createDOM('div', "item-title")
 		const nameDOM = createDOM('h2', "item-name");
-		const descDOM = createDOM('p', "item-desc");
+		const priceDOM = createDOM('p', "item-price")
 		nameDOM.innerText = name;
+		priceDOM.innerText = '$' + price;
+		parent.append(nameDOM, priceDOM);
+		return parent
+	}
+
+	const addDesc = (desc) => {
+		const descDOM = createDOM('p', "item-desc");
 		descDOM.innerText = desc;
+		return descDOM;
+	}
+	const addText = (item) => {
+		const base = createDOM('div', "item-text");
+		const nameDOM = addTitle(item.name, item.price);
+		const descDOM = addDesc(item.desc);
 		base.append(nameDOM, descDOM);
 		return base;
 	}
 	const addImg = (img) => {
-		if (img == undefined) return "";
+		if (img == undefined || !img) return "";
 		const parent = createDOM('div', "img-base");
 		const imgDOM = new Image();
 		imgDOM.src = require('./img/' + img);
@@ -29,17 +42,17 @@ const card = (() => {
 	const create = (item) => {
 		const card = document.createElement('div');
 		card.classList.add("menu-item")
-		card.append(addImg(item.img), addText(item.name, item.desc));
+		card.append(addImg(item.img), addText(item));
 		return card;
 	}
 	return {
 		create,
 	}
-})()
+})();
 
 function render() {
 	const base = init();
-	menu.forEach(item => base.appendChild(card.create(item)))
+	menu.forEach(item => base.appendChild(card.create(item)));
 	const container = createContainer();
 	container.appendChild(base);
 	return container;
